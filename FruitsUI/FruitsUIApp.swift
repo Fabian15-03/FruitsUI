d9ada8732a7d5e709d01456fc035bb11ade557e5
Fabian15-03 <luminition@hotmail.com>
2023-06-09 20:29:26 -0500

    Initial Commit

---
+++ FruitsUI/FruitsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsUIApp.swift
+//  FruitsUI
+//
+//  Created by Fabian Gonzales  on 9/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
